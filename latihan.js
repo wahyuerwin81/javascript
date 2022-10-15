abf77807cf46a1a6f2fc8c1d0259c42fa967e4e3
wahyuerwin81 <wahyuerwin81@gmail.com>
2022-10-15 21:26:39 +0700

    first commit

---
+++ latihan.js
@@ -0,0 +1,26 @@
+ //operator aritmatika
+// 3 variable
+var angkaPertama = 20;
+var    angkaKedua   = 10;
+  
+
+// penjumlahan
+
+ var total = angkaPertama + angkaKedua
+console. log(total)
+
+//pengurangan
+
+var total = angkaPertama - angkaKedua
+console. log(total)
+
+//perkalian
+
+var total = angkaPertama * angkaKedua
+console. log(total)
+
+//pembagian
+
+var total = angkaPertama / angkaKedua
+console. log(total)
+
